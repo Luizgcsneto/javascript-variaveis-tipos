6e3cea3f51404ab5f9c0a3cefd5eb3ddfc70f52f
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-05 23:32:14 -0300

    função de verificar palindromo

---
+++ Aula 1 - Atribuindo Valores/script.js
@@ -0,0 +1,14 @@
+// let nome = 'luiz gonzaga da costa souza neto'
+// let sobrenome = 'gonzaga'
+
+// nome.concat(' ',sobrenome) // concatecar uma string
+
+// console.log(nome.split(","))
+
+function verificarPalindromo(string){
+    if(!string) return
+
+    return string.split('').reverse().join('') === string
+}
+
+console.log(verificarPalindromo('arara'))
