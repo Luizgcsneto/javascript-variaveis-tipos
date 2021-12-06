867cbb1398437e2b2aab52e1c106cf166271ce15
Luiz Neto <luizgonzaga15@gmail.com>
2021-12-06 00:14:08 -0300

    outra função de palindromo

--- Aula 1 - Atribuindo Valores/script.js
+++ Aula 1 - Atribuindo Valores/script.js
@@ -3,12 +3,24 @@
 
  nome.concat(' ',sobrenome) // concatecar uma string
 
- console.log(nome.split(","))
+ //console.log(nome.split(","))
 
 function verificarPalindromo(string){
-    if(!string) return
+    if(!string) return 'string inexistente'
 
     return string.split('').reverse().join('') === string
 }
 
+function verificarPalindromo2(string){
+    if(!string) return 'string inexistente'
+
+    for(let i=0; i < string.length; i++){
+        if(string[i] !== string[string.length -1 -i]) return false; 
+    }
+
+    return true;
+}
+
 console.log(verificarPalindromo('arara'))
+
+console.log(verificarPalindromo2('omo'))
